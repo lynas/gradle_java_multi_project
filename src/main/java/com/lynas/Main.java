a9e55c4a285583d3d6bae9bf8bdc7e470dc00090
LynAs Sazzad <szlynas@gmail.com>
2015-03-16 14:54:44 +0600

    initial commit with all file

---
+++ src/main/java/com/lynas/Main.java
@@ -0,0 +1,11 @@
+package com.lynas;
+
+/**
+ * Created by lynas on 3/16/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.changeCase("hellow World"));
+    }
+}
